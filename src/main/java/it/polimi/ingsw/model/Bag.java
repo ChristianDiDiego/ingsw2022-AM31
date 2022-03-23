21aaee76d72e538521479bf23c7f39805167c7c7
Federica Di Filippo <federica.difilippo@mail.polimi.it>
2022-03-23 16:13:49 +0100

    added Bag

---
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.model;
+
+import java.util.Random;
+
+public class Bag {
+    private int[] studentsInBag;
+    private int numberPlayer;
+
+    public Bag(int numberPlayer){
+        this.numberPlayer= numberPlayer;
+        studentsInBag= new int[5];
+        for(int i=0; i<5; i++){
+            studentsInBag[i] = 26;
+        }
+    }
+
+    public int getNumberOfLeftStudents(){
+        int temp = 0;
+        for(int i=0; i<5; i++){
+            temp = temp +studentsInBag[i];
+        }
+        return temp;
+    }
+
+    public int[] pickStudent(){
+        int[] studentsToPick = new int[5];
+        for(int i=0; i<5;i++){
+            studentsToPick[i]=0;
+        }
+
+        Random random = new Random();
+        if(numberPlayer==4) {
+            for (int i = 0; i<3; i++) {
+                int value = random.nextInt(5 + 0);
+                studentsToPick[i]++;
+                studentsInBag[i]--;
+            }
+        }else {
+            for (int i = 0; i < 3; i++) {
+                int value = random.nextInt(5 + 0);
+                studentsToPick[i]++;
+                studentsInBag[i]--;
+            }
+
+        }
+        return studentsToPick;
+    }
+}
