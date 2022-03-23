e5705b2b91567c2601a0b778870a9466c09a0d76
Carmine Faino <carmine.faino@mail.polimi.it>
2022-03-23 15:50:00 +0100

    added Island, Cloud classes

--- src/main/java/it/polimi/ingsw/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/Cloud.java
@@ -1,4 +1,48 @@
 package it.polimi.ingsw.model;
 
 public class Cloud {
+    private int idCloud;
+    private int[] studentOnCloud = new int[5];
+    private boolean isTaken;
+
+    public Cloud(int idCloud) {
+        this.idCloud = idCloud;
+        isTaken = false;
+        for(int i = 0; i < 5; i++) {
+            studentOnCloud[i] = 0;
+        }
+    }
+
+    public int getIdCloud() {
+        return idCloud;
+    }
+
+    public int[] getStudents() {
+        return studentOnCloud;
+    }
+
+    public boolean getIsTaken() {
+        return isTaken;
+    }
+
+    public void addStudents(int[] toAdd) {
+        for(int i = 0; i < 5; i++) {
+            studentOnCloud[i] += toAdd[i];
+        }
+    }
+
+    public void removeStudents() {
+        for(int i = 0; i < 5; i++) {
+            studentOnCloud[i] = 0;
+        }
+    }
+
+    public void changeStatus() {
+        if (isTaken == false) {
+            this.isTaken = true;
+        } else {
+            this.isTaken = false;
+        }
+    }
+
 }
---
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.model;
+
+public class Island {
+    private int idIsland;
+    private int[] studentOnIsland = new int[5];
+    private Player owner;
+
+    public Island(int idIsland) {
+        this.idIsland = idIsland;
+        for(int i = 0; i < 5; i++) {
+            studentOnIsland[i] = 0;
+        }
+    }
+
+    public int getIdIsland() {
+        return idIsland;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public int[] getAllStudents() {
+        return studentOnIsland;
+    }
+
+    public int getStudentsByColor(int color) {
+        return studentOnIsland[color];
+    }
+
+    public void addStudent(int color) {
+        studentOnIsland[color] += 1;
+    }
+
+    public void changeOwner(Player player) {
+        this.owner = player;
+    }
+}
