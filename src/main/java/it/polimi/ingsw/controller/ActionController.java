1e79d15713aec7e38303748ac808a2230810b77f
ChristianDD <christian.didiego@mail.polimi.it>
2022-04-01 20:54:28 +0200

    Added checkActionCard

--- src/main/java/it/polimi/ingsw/controller/ActionController.java
+++ src/main/java/it/polimi/ingsw/controller/ActionController.java
@@ -8,17 +8,20 @@ import static it.polimi.ingsw.model.ColorOfTower.BLACK;
 
 //TODO: Checks need to be finished checking that the actions are correct
 // (enough players in entrance to be moved etc.)
+// Add tests!!!!!!
 
 public class ActionController {
     private Phase phase;
     private Game game;
     private int isFinished;
     private ActionParser actionParser;
+    private TurnController turnController;
 
-    public ActionController(Game game){
+    public ActionController(Game game, TurnController turnController){
         this.game = game;
         isFinished = 0;
         actionParser = new ActionParser(this);
+        this.turnController = turnController;
     }
 
     public Phase getPhase() {
@@ -39,12 +42,23 @@ public class ActionController {
 
     }
 
+    public void checkActionCard(Player player, int power){
+        if(game.getPhase()== Phase.CARD_SELECTION && player == game.getCurrentPlayer()){
+            turnController.manageReceivedCard(player, power);
 
+        }else if(game.getPhase()== Phase.CARD_SELECTION || player != game.getCurrentPlayer()){
+            System.out.println("non è il tuo turno!!");
+        }else if(game.getPhase()!= Phase.CARD_SELECTION && player == game.getCurrentPlayer()){
+            System.out.println("hai inviato un'azione non valida, riprova");
+        }
+    }
+
+//TODO: check if the destination is valid
    public void checkActionMoveStudent(Player player,StudsAndProfsColor[] colors, int[] destinations){
         if(game.getPhase()== Phase.MOVE_STUDENTS && player == game.getCurrentPlayer()){
             //check che l'azione sia valida, in caso aggiorno Model
 
-        }else if(game.getPhase()== Phase.MOVE_STUDENTS && player != game.getCurrentPlayer()){
+        }else if(game.getPhase()== Phase.MOVE_STUDENTS || player != game.getCurrentPlayer()){
             System.out.println("non è il tuo turno!!");
        }else if(game.getPhase()!= Phase.MOVE_STUDENTS && player == game.getCurrentPlayer()){
             System.out.println("hai inviato un'azione non valida, riprova");
@@ -55,7 +69,7 @@ public class ActionController {
         if(game.getPhase()== Phase.MOVE_MN && player == game.getCurrentPlayer()){
             //check che l'azione sia valida, in caso modifico il model
 
-        }else if(game.getPhase()== Phase.MOVE_MN && player != game.getCurrentPlayer()){
+        }else if(game.getPhase()== Phase.MOVE_MN || player != game.getCurrentPlayer()){
             System.out.println("non è il tuo turno!!");
         }else if(game.getPhase()!= Phase.MOVE_MN && player == game.getCurrentPlayer()){
             System.out.println("hai inviato un'azione non valida, riprova");
@@ -71,7 +85,7 @@ public class ActionController {
         if(game.getPhase()== Phase.CLOUD_SELECTION && player == game.getCurrentPlayer()){
             //check che l'azione sia valida, in caso modifico il model
 
-        }else if(game.getPhase()== Phase.CLOUD_SELECTION && player != game.getCurrentPlayer()){
+        }else if(game.getPhase()== Phase.CLOUD_SELECTION || player != game.getCurrentPlayer()){
             System.out.println("non è il tuo turno!!");
         }else if(game.getPhase()!= Phase.CLOUD_SELECTION && player == game.getCurrentPlayer()){
             System.out.println("hai inviato un'azione non valida, riprova");
@@ -84,66 +98,6 @@ public class ActionController {
         return game.getCurrentPlayer();
     }
     //questa chiama quella sopra
-
-    /**
-     * Calculate the influence on the archipelago where MN is present
-     * Menage the tower situation of an archipelago and of the players after that
-     * Call checkUnification to check if the archipelago needs to be unified to another one
-     */
-    public void calculateInfluence(){
-        for(Archipelago a: game.getListOfArchipelagos()){
-            if(a.getIsMNPresent()){
-                Player newOwner;
-                Player oldOwner;
-                int maxInfluence =0;
-                if(a.getOwner() == null){
-                    oldOwner = null;
-                    newOwner = game.getCurrentPlayer();
-
-                }else {
-                    oldOwner = a.getOwner();
-                    newOwner = a.getOwner();
-                    maxInfluence = a.getBelongingIslands().size();
-                }
-
-                for(int c=0; c< Constants.NUMBEROFKINGDOMS; c++){
-                    for(Island i: a.getBelongingIslands()){
-                        if(i.getAllStudents()[c] > 0 && a.getOwner().getMyBoard().getProfessorsTable().getHasProf(StudsAndProfsColor.values()[c])) {
-                            maxInfluence += i.getAllStudents()[c];
-                        }
-                    }
-                }
-                for(Player p : game.getOrderOfPlayers()){
-                    if(p != newOwner){
-                        int newInfluence = 0;
-                        for(int c=0; c< Constants.NUMBEROFKINGDOMS; c++){
-                            for(Island i: a.getBelongingIslands()){
-                                if(i.getAllStudents()[c] > 0 && p.getMyBoard().getProfessorsTable().getHasProf(StudsAndProfsColor.values()[c])) {
-                                    newInfluence += i.getAllStudents()[c];
-                                }
-                            }
-
-                        }
-                        if(newInfluence > maxInfluence){
-                            newOwner = p;
-                            maxInfluence = newInfluence;
-                        }
-                    }
-                }
-
-                if(maxInfluence > 0){
-                    a.changeOwner(newOwner);
-                    for(int i = 0; i < a.getBelongingIslands().size(); i++) {
-                        if(oldOwner != null){
-                            oldOwner.getMyBoard().getTowersOnBoard().removeTower();
-                        }
-                        newOwner.getMyBoard().getTowersOnBoard().removeTower();
-                    }
-                    checkUnification(a);
-                }
-            }
-        }
-    }
 
     /**
      * Check if an archipelago has to be unified with previous or next,
