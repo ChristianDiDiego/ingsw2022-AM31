e28f866313fda45bcbe215c92f729c398e0950b3
ChristianDD <christian.didiego@mail.polimi.it>
2022-04-02 19:32:06 +0200

    Added game parameter

--- src/main/java/it/polimi/ingsw/model/expertMode/Character4.java
+++ src/main/java/it/polimi/ingsw/model/expertMode/Character4.java
@@ -1,13 +1,15 @@
 package it.polimi.ingsw.model.expertMode;
 
+import it.polimi.ingsw.model.Game;
+
 /**
  * image 7 in image folder ;
  * card 8 in list of cards ( in game tutorial)
  * When you calculate the influence, the player who play this card has 2 additional points
  */
 public class Character4 extends Characters{
-    public Character4() {
-        super(2);
+    public Character4(Game game) {
+        super(2, game);
     }
     @Override
     public void usePower() {
