ad847474bed320c54a761e888e84561039fb5e3d
ChristianDD <christian.didiego@mail.polimi.it>
2022-04-02 19:31:45 +0200

    Added javadoc

--- src/main/java/it/polimi/ingsw/model/expertMode/Characters.java
+++ src/main/java/it/polimi/ingsw/model/expertMode/Characters.java
@@ -1,13 +1,22 @@
 package it.polimi.ingsw.model.expertMode;
 
-public abstract class Characters {
+import it.polimi.ingsw.model.Game;
 
+/**
+ * Every character has the function usePower which is called by the actionController
+ * ActionController check if a player played a character when a player use a card
+ * TODO:add charactersUsed to the player ; add game parameter to all characters
+ * turnController set characterUsed of the player to null at the beginning of the turn
+ */
+public abstract class Characters {
+    private Game game;
     private  int price;
     private boolean alreadyUsed;
 
-    public Characters(int price){
+    public Characters(int price, Game game){
         alreadyUsed = false;
         this.price = price;
+        this.game = game;
     }
 
     public abstract void usePower();
