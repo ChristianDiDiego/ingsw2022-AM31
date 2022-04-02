a6a8af3e77cf441c6af2bef03fe6de4828966da7
Carmine Faino <carmine.faino@mail.polimi.it>
2022-04-02 11:59:22 +0200

    Modify cli

--- src/main/java/it/polimi/ingsw/client/cli/Cli.java
+++ src/main/java/it/polimi/ingsw/client/cli/Cli.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.client.cli;
 
 import com.sun.tools.javac.code.Attribute;
 import it.polimi.ingsw.client.View;
+import it.polimi.ingsw.constants.Constants;
 import it.polimi.ingsw.model.*;
 
 import java.util.List;
@@ -37,12 +38,12 @@ public class Cli implements View {
     public void printLastUsedCards(List<Player> players) {
         int min = 10;
         int i;
-        for (i = 0; i < players.size() - 1; i++) {
+        for (i = 0; i < players.size(); i++) {
             if (players.get(i).getMyDeck().getLeftCards().size() < min) {
                 min = players.get(i).getMyDeck().getLeftCards().size();
             }
         }
-        for (i = 0; i < players.size() - 1; i++) {
+        for (i = 0; i < players.size(); i++) {
             if(players.get(i).getMyDeck().getLeftCards().size() == min) {
                 System.out.println("Player " + players.get(i).getNickname() + " choose the card:" + "\n");
                 System.out.println("Power: " + players.get(i).getLastUsedCard().getPower() + " Steps: " + players.get(i).getLastUsedCard().getMaxSteps());
@@ -58,7 +59,7 @@ public class Cli implements View {
             int[] nSE = new int[]{0,0,0,0,0};
             int nT = p.getMyBoard().getTowersOnBoard().getNumberOfTowers();
             boolean[] hasProf =  new boolean[]{false, false, false, false, false};
-            for(int i=0; i<5; i++){
+            for(int i = 0; i < Constants.NUMBEROFKINGDOMS; i++){
                 nSDN[i] = p.getMyBoard().getDiningRoom().getStudentsByColor(StudsAndProfsColor.values()[i]);
                 nSE[i] = p.getMyBoard().getEntrance().getStudentsByColor(StudsAndProfsColor.values()[i]);
                 hasProf[i] = p.getMyBoard().getProfessorsTable().getHasProf(StudsAndProfsColor.values()[i]);
@@ -67,10 +68,10 @@ public class Cli implements View {
             StringBuilder board = new StringBuilder();
             board.append(ColorsCli.BLACK).append("\n Board of player: "+ p + "\n").append(ColorsCli.BLACK);
             for(int j=0 ; j<5 ; j++){
-                for(int k = 0; k<nSDN[j]; k++){
+                for(int k = 0; k < nSDN[j]; k++){
                     board.append(ColorsCli.getColorByNumber(j)).append(" ●").append(ColorsCli.getColorByNumber(j));
                 }
-                for(int k=nSDN[j]; k<10; k++){
+                for(int k = nSDN[j]; k < 10; k++){
                     board.append(ColorsCli.getColorByNumber(j)).append(" ◯").append(ColorsCli.getColorByNumber(j));
                 }
                 if(hasProf[j]){
@@ -82,14 +83,14 @@ public class Cli implements View {
             }
 
             board.append(ColorsCli.BLACK).append("Students in entrance: \n").append(ColorsCli.BLACK);
-            for(int i=0; i<5; i++){
+            for(int i = 0; i < Constants.NUMBEROFKINGDOMS; i++){
                 for(int k=0; k<nSE[i]; k++){
                     board.append(ColorsCli.getColorByNumber(i)).append("● ").append(ColorsCli.getColorByNumber(i));
                 }
                 board.append(ColorsCli.BLACK).append("\n").append(ColorsCli.BLACK);
             }
             board.append(ColorsCli.BLACK).append("Towers on board: \n").append(ColorsCli.BLACK);
-            for(int i = 0; i<nT; i++){
+            for(int i = 0; i < nT; i++){
                 board.append(ColorsCli.BLACK).append("♜ ").append(ColorsCli.BLACK).append(ColorsCli.RESET);
             }
             System.out.println(board.toString());
@@ -99,53 +100,45 @@ public class Cli implements View {
 
     @Override
     public void printArchipelagos(List<Archipelago> archipelagos) {
-        int red, green, yellow, pink, blue;
-        System.out.println(
-                "ARCHIPELAGOS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n");
-        for(int c = 0; c < archipelagos.size() - 1; c++) {
-            System.out.println("Id archipelago: " + archipelagos.get(c).getIdArchipelago() + "\n");
-            red = 0;
-            green = 0;
-            yellow = 0;
-            pink = 0;
-            blue = 0;
-            for(Island i : archipelagos.get(c).getBelongingIslands()) {
-                red += i.getStudentsByColor(StudsAndProfsColor.RED);
-                green += i.getStudentsByColor(StudsAndProfsColor.GREEN);
-                yellow += i.getStudentsByColor(StudsAndProfsColor.YELLOW);
-                pink += i.getStudentsByColor(StudsAndProfsColor.PINK);
-                blue += i.getStudentsByColor(StudsAndProfsColor.BLUE);
-            }
-            System.out.println("Red: " + red + "\n");
-            System.out.println("Green: " + green + "\n");
-            System.out.println("Yellow: " + yellow + "\n");
-            System.out.println("Pink: " + pink + "\n");
-            System.out.println("Blue: " + blue + "\n");
+        StringBuilder archipelago = new StringBuilder();
+        archipelago.append(ColorsCli.RESET).append("ARCHIPELAGOS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").append(ColorsCli.RESET);
+        System.out.println(archipelago.toString());
+        for(int i = 0; i < archipelagos.size(); i++) {
+                archipelago = new StringBuilder();
+                archipelago.append(ColorsCli.RESET).append("Id archipelago: " + archipelagos.get(i).getIdArchipelago() + " owner: " + archipelagos.get(i).getOwner().getNickname() + "\n").append(ColorsCli.RESET);
+                for(Island is : archipelagos.get(i).getBelongingIslands()) {
+                    for(int j = 0; j < Constants.NUMBEROFKINGDOMS; j++){
+                        for(int k = 0; k < is.getStudentsByColor(StudsAndProfsColor.values()[j]); k++){
+                            archipelago.append(ColorsCli.getColorByNumber(j)).append("● ").append(ColorsCli.getColorByNumber(j));
+                        }
+                    }
+                    System.out.println(archipelago.toString());
+                }
         }
-        System.out.println(
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n");
+        archipelago = new StringBuilder();
+        archipelago.append(ColorsCli.RESET).append("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").append(ColorsCli.RESET);
+        System.out.println(archipelago.toString());
     }
 
     @Override
     public void printClouds(List<Cloud> clouds) {
-        System.out.println(
-                "CLOUDS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n"
-        );
-        for(int i = 0; i < clouds.size() - 1; i++) {
+        StringBuilder cloud = new StringBuilder();
+        cloud.append(ColorsCli.RESET).append("CLOUDS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").append(ColorsCli.RESET);
+        System.out.println(cloud.toString());
+        for(int i = 0; i < clouds.size(); i++) {
             if(clouds.get(i).getIsTaken() == false) {
-                System.out.println("Id cloud: " + clouds.get(i).getIdCloud() + "\n");
-                System.out.println("Red: " + clouds.get(i).getStudents()[0] + "\n");
-                System.out.println("Green: " + clouds.get(i).getStudents()[1] + "\n");
-                System.out.println("Yellow: " + clouds.get(i).getStudents()[2] + "\n");
-                System.out.println("Pink: " + clouds.get(i).getStudents()[3] + "\n");
-                System.out.println("Blue: " + clouds.get(i).getStudents()[4] + "\n");
+                cloud = new StringBuilder();
+                cloud.append(ColorsCli.RESET).append("Id cloud: " + clouds.get(i).getIdCloud() + "\n").append(ColorsCli.RESET);
+                for(int j = 0; j < Constants.NUMBEROFKINGDOMS; j++){
+                    for(int k = 0; k < clouds.get(i).getStudents()[j]; k++){
+                        cloud.append(ColorsCli.getColorByNumber(j)).append("● ").append(ColorsCli.getColorByNumber(j));
+                    }
+                }
+                System.out.println(cloud.toString());
             }
         }
-        System.out.println(
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n"
-        );
+        cloud = new StringBuilder();
+        cloud.append(ColorsCli.RESET).append("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").append(ColorsCli.RESET);
+        System.out.println(cloud.toString());
     }
 }
-
-
-
