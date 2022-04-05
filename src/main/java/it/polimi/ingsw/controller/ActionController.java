28de13c935be5488562f5d81a90cc5c483c8c774
Federica Di Filippo <federica.difilippo@mail.polimi.it>
2022-04-05 16:12:19 +0200

    fixed checkActionCloud

--- src/main/java/it/polimi/ingsw/controller/ActionController.java
+++ src/main/java/it/polimi/ingsw/controller/ActionController.java
@@ -245,6 +245,7 @@ public class ActionController {
                 if (cloud.getIdCloud() == cloudId){
                     if(!cloud.getIsTaken()){
                         player.getMyBoard().getEntrance().addStudent(cloud.getStudents());
+                        cloud.removeStudents();
                         game.getCurrentPlayer().setUsedCharacter(0);
                         game.nextPhase();
                         return true;
