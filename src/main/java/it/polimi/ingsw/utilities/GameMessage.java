9b9d59bb1c19601e0fd26d56e43da2f03d77d002
FedericaDiFilippo <federica.difilippo@mail.polimi.it>
2022-06-27 23:03:28 +0200

    cleaned messages

--- src/main/java/it/polimi/ingsw/utilities/GameMessage.java
+++ src/main/java/it/polimi/ingsw/utilities/GameMessage.java
@@ -5,9 +5,9 @@ package it.polimi.ingsw.utilities;
  */
 public class GameMessage {
     public static final String cardSelectionMessage = "Play a card of your deck \n writing CARD [Number of the card]";
-    public static String studentMovementMessage = "move %d of your students from entrance \n please type MOVEST [COLOR]-[DESTINATION],[COLOR]-[DESTINATION].. \nwhere Color is: the starting letter of the color:\nY for yellow, B for blue...\nDestination is:\n0: Board\n1-12: Archipelagos' id";
-    public static String moveMotherNatureMessage = "move mother nature; For this round you can do max %d steps \n please type MOVEMN [STEPSOFMN]\n";
-    public static final String chooseCloudMessage = "choose a cloud \n please type CLOUD [NUMBEROFCLOUDTOPICK]\n";
+    public static String studentMovementMessage = "Move %d of your students from entrance \n please type MOVEST [COLOR]-[DESTINATION],[COLOR]-[DESTINATION].. \nwhere Color is: the starting letter of the color:\nY for yellow, B for blue...\nDestination is:\n0: Board\n1-12: Archipelagos' id";
+    public static String moveMotherNatureMessage = "Move mother nature: for this round you can do max %d steps \n please type MOVEMN [STEPSOFMN]\n";
+    public static final String chooseCloudMessage = "Choose a cloud \n please type CLOUD [NUMBEROFCLOUDTOPICK]\n";
     public static final String characterInstructions = "To select a character push the corresponding image. If the character's action requires some specifications, there will be a menu on the right (you must first select the target in the menu and then click on the character)";
     public static final String specialCommand = "\n\nAt any time you can use the command \"quit\" to quit the game \nand the command \"show deck\" to see your remaining cards\n\n";
 }
